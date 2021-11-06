8a80dd3394c4337e1dea550d5afa2e5a31be4aec
dev <devjutzu@gmail.com>
2021-11-05 22:09:16 -0400

    Updates

---
+++ precache-manifest.b2bd6434bcf54408e69f3bbad9bfa949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "676f0fb59dfcd3dc003944be2df350cc",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "f989ad2d6f0e163a7cf7",
+    "url": "/pomodoro/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "8f60e831c49dd590640b",
+    "url": "/pomodoro/static/js/2.0acf020f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pomodoro/static/js/2.0acf020f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f989ad2d6f0e163a7cf7",
+    "url": "/pomodoro/static/js/main.68266b52.chunk.js"
+  },
+  {
+    "revision": "a9aee71dab78a19abc00",
+    "url": "/pomodoro/static/js/runtime-main.50bf0b17.js"
+  }
+]);
