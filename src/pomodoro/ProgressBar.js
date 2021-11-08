9fb5ee7d8dac1d04e54f819af4347c99f321b3e3
Parth Patel <Parth.patel.r@gmail.com>
2021-11-08 10:27:26 -0500

    Update ProgressBar.js

--- src/pomodoro/ProgressBar.js
+++ src/pomodoro/ProgressBar.js
@@ -1,17 +1,8 @@
 import React from "react";
 
 const ProgressBar= ({session,breakDuration,focusDuration})=> {
-    return (
-      <div className="col">
-        <div className="progress" style={{ height: "20px" }}>
-          <div
-            className="progress-bar"
-            role="progressbar"
-            aria-valuemin="0"
-            aria-valuemax="100"
-            aria-valuenow={
-              session
-                ? (Math.abs(
+    function calcWidth(){
+        return Math.abs(
                     (session.label === "Focusing"
                       ? focusDuration
                       : breakDuration) *
@@ -23,23 +14,23 @@ const ProgressBar= ({session,breakDuration,focusDuration})=> {
                       : breakDuration) *
                       60)) *
                   100
+    }
+    return (
+      <div className="col">
+        <div className="progress" style={{ height: "20px" }}>
+          <div
+            className="progress-bar"
+            role="progressbar"
+            aria-valuemin="0"
+            aria-valuemax="100"
+            aria-valuenow={
+              session
+                ? calcWidth()
                 : "0"
             } // TODO: Increase aria-valuenow as elapsed time increases
             style={{
               width: session
-                ? (Math.abs(
-                    (session.label === "Focusing"
-                      ? focusDuration
-                      : breakDuration) *
-                      60 -
-                      session.timeRemaining
-                  ) /
-                    ((session.label === "Focusing"
-                      ? focusDuration
-                      : breakDuration) *
-                      60)) *
-                    100 +
-                  "%"
+                ? calcWidth()+"%"
                 : "0%",
             }} // TODO: Increase width % as elapsed time increases
           />
